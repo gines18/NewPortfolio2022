a5fb25c6b40a5d842f94a18895b68f938bb75333
gines18 <ruczkowski.m@gmail.com>
2023-05-23 10:00:01 +0100

    add eventlistener for contact form

---
+++ script.js
@@ -0,0 +1,6 @@
+let button = document.getElementById('button')
+
+button.addEventListener('click', () => {
+alert('This contact form is not working yet! You will find my email on my github page.')
+});
+
